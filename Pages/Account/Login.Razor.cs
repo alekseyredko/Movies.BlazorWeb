b6ca7f31caf74e43886b62b7f80a45e212c17371
alekseyredko <aliakseiredzko@gmail.com>
2021-07-21 17:11:27 +0300

    refactor login

---
+++ Pages/Account/Login.Razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Movies.Data.Results;
+using Movies.Infrastructure.Models;
+using Movies.Infrastructure.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.BlazorWeb.Pages.Account
+{
+    public partial class Login
+    {
+        [Inject]
+        ICustomAuthentication customAuthentication { get; set; }
+        
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+
+        private string login { get; set; }
+        private string password { get; set; }
+
+        private Result<LoginUserResponse> response;
+
+        [CascadingParameter] 
+        Task<AuthenticationState> authenticationStateTask { get; set; }
+
+        private async Task LogUsername()
+        {
+            await authenticationStateTask;
+
+            var userRequest = new LoginUserRequest()
+            {
+                Login = login,
+                Password = password
+            };
+            response = await customAuthentication.TryLoginAsync(userRequest);
+            if (response.ResultType == ResultType.Ok)
+            {
+                NavigationManager.NavigateTo("/");
+            }
+        }
+    }
+}
