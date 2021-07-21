91dcf2d46f76477547f29d8b67ec195a53fc96cd
alekseyredko <aliakseiredzko@gmail.com>
2021-07-21 13:30:18 +0300

    change component to partial class

---
+++ Pages/MoviesPages/ShowMovies.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Movies.Data.Models;
+using Movies.Data.Results;
+using Movies.Data.Services.Interfaces;
+using Movies.Infrastructure.Models.User;
+using Movies.Infrastructure.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.BlazorWeb.Pages.MoviesPages
+{    
+    public partial class ShowMovies: ComponentBase
+    {
+        private Result<IEnumerable<Movie>> _movies;
+        private Result<GetUserResponse> _currentUser;
+        private bool _canShowEdit;        
+
+        [Inject]
+        private ICustomAuthentication _customAuthentication { get; set; }
+
+        [Inject]
+        private IMovieService _movieService { get; set; }
+
+        [Inject]
+        private NavigationManager _navigationManager { get; set; }        
+
+        protected override async Task OnInitializedAsync()
+        {
+            _movies = await _movieService.GetAllMoviesAsync();
+            _currentUser = await _customAuthentication.GetCurrentUserDataAsync();
+            
+            if (_currentUser.ResultType == ResultType.Ok)
+            {
+                _canShowEdit = _currentUser.Value.Roles.Contains(UserRoles.Producer);
+            }
+        }
+    }
+}
